a45765e731566fd363a5faef8d567a5982317c52
f22linbe <f22linbe@student.his>
2023-05-19 18:16:47 +0200

    Title added

--- app/src/main/java/com/example/animalislands/MainActivity.java
+++ app/src/main/java/com/example/animalislands/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -25,5 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
